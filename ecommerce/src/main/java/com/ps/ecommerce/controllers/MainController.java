27f668a61394f0d608bf44a8b3727a55c26fb033
Asif <asif2346@gmail.com>
2024-01-03 19:04:08 +0530

    Initial code2

--- ecommerce/src/main/java/com/ps/ecommerce/controllers/MainController.java
+++ ecommerce/src/main/java/com/ps/ecommerce/controllers/MainController.java
@@ -128,9 +128,10 @@ public class MainController {
 
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+            return "index";
         }
         return "index";
     }
